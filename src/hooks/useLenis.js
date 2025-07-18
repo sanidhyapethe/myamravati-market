25cdbf4c35d8402e2f0ba1171c593ee2d7561b91
sanidhya pethee <sanipethe22@gmail.comm>
2025-07-18 11:56:52 +0530

    add hook folder and uselenis file

---
+++ src/hooks/useLenis.js
@@ -0,0 +1,24 @@
+// src/hooks/useLenis.js
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+
+export default function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Ease-out
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy(); // clean up on unmount
+    };
+  }, []);
+}
